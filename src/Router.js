fcf1c73e6d66ca9013d4b296868677d6ffc83136
Ryo <kanadajinlee@yahoo.ca>
2017-10-02 10:42:55 -0400

    hash router time

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Route,
   Switch
 } from "react-router-dom";
@@ -15,7 +15,6 @@ export default () => (
       <Route path="/" exact component={Home}/>
       <Route path="/home" exact component={Home}/>
       <Route path="/users/create" exact component={Create}/>
-      {/* アップデート */}
       <Route path="/users/:id" exact component={Profile}/>
       <Route path="/users/:id/edit" exact component={Edit}/>
     </Switch>
